c6ddd6f64f9a8f5acfd8db5735a270ac64afbf51
Rich Hosek <richhosek@gmail.com>
2021-07-17 09:21:33 -0500

    Added Lamppost model and removed old template models

--- models/index.js
+++ models/index.js
@@ -1,25 +1,3 @@
-const Traveller = require('./Traveller');
-const Location = require('./Location');
-const Trip = require('./Trip');
+const Lamppost = require('./Lamppost');
 
-Traveller.belongsToMany(Location, {
-  // Define the third table needed to store the foreign keys
-  through: {
-    model: Trip,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'planned_trips'
-});
-
-Location.belongsToMany(Traveller, {
-  // Define the third table needed to store the foreign keys
-  through: {
-    model: Trip,
-    unique: false
-  },
-  // Define an alias for when data is retrieved
-  as: 'location_travellers'
-});
-
-module.exports = { Traveller, Location, Trip };
+module.exports = { Lamppost };
